c77c42777bfb2916168f751d299f3a2af1be0eb2
alex-chernenko <38989176+alex-chernenko@users.noreply.github.com>
2018-07-15 14:30:39 +0300

    Fixed routes

--- routes/api/routes.js
+++ routes/api/routes.js
@@ -1,7 +1,40 @@
 const message = require("./message");
 const user = require("./user");
+var User = require('../../models/user.js');
+var Mes = require('../../models/message.js');
 module.exports = function(app) {
-  app.use("/message", message);
-  
-app.use("/api", user);
+
+    
+        
+    app.get('/history/:id', function(req, res, next) {
+            Mes.find({ senderId: req.params.id}, function (err, post) {
+              
+               let l = JSON.stringify(post, function(key, value) {
+                  if (key === 'senderId') {
+                    return undefined; 
+                  }
+                  if (key === '_id') {
+                    return undefined; 
+                  }
+                  if (key === 'text') {
+                    return undefined; 
+                  }
+                  if (key === '__v') {
+                    return undefined; 
+                  }
+                  else {
+                  return value;
+                  }
+                }); 
+             
+             
+              if (err) return next(err);
+                
+              
+              res.send(l);
+            });
+
+           
+      });
+app.use("/api",user,message);
 };
